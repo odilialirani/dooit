2c94cc0dd73d9dee623af34e707af5b1896b17f1
odilialirani <oclirani@gmail.com>
2020-04-25 20:11:10 -0500

    add user to db

--- dooit/app/models/user.rb
+++ dooit/app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+  self.table_name = 'users'
+
+  validates :username, presence: true
+  validates :password, presence: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
 end
--- dooit/db/migrate/20200426004514_create_users.rb
+++ dooit/db/migrate/20200426004514_create_users.rb
@@ -1,8 +1,11 @@
 class CreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
-
-      t.timestamps
+      t.string :username, null: false
+      t.string :password, null: false
+      t.string :first_name, null: false
+      t.string :last_name, null: false
+      t.timestamps null: false
     end
   end
 end
