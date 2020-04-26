197d59429dd3b29ed9a8637fe1aca404b9f82be0
odilialirani <oclirani@gmail.com>
2020-04-26 15:24:00 -0500

    users view

--- dooit/db/seeds.rb
+++ dooit/db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+9.times do |i|
+  User.create(
+    username: "dooituser#{ i + 1 }",
+    password: SecureRandom.hex,
+    first_name: "user#{ i + 1 }",
+    last_name: "dooit#{ i + 1 }"
+  )
+end
