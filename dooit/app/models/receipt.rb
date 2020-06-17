33c826f23b6c8df4c6d3cc777fb88c6bf9529c53
odilialirani <oclirani@gmail.com>
2020-06-16 21:48:18 -0500

    add read attr for receipt

--- dooit/app/models/receipt.rb
+++ dooit/app/models/receipt.rb
@@ -1,11 +1,14 @@
 class Receipt < ApplicationRecord
   self.table_name = 'receipts'
 
+  attr_accessible :s3_bucket, :s3_path
+
   belongs_to :user
 
   has_many :items
 
   def process_image
-    
+    tesseract = Split::Tesseract.new(self)
+    tesseract.process
   end
 end
