80e6af90bff3956c87501bab85d6f83ee4acdbd0
odilialirani <oclirani@gmail.com>
2020-06-16 21:48:41 -0500

    tesseract read data from s3

--- dooit/lib/split/tesseract/tesseract.rb
+++ dooit/lib/split/tesseract/tesseract.rb
@@ -1,5 +1,30 @@
-module Dooit
+require 'aws-sdk'
+
+module Split
   class Tesseract
 
+    def initialize(receipt)
+      @receipt = receipt
+    end
+
+    def process
+      s3 = Aws::S3::Client.new
+      object = s3.get_object(bucket: receipt.s3_bucket, key: receipt.s3_path)
+      body = object.body.read
+
+      # Run the gem
+
+      # Get the data & save to db
+      # - location
+      # - date
+      # - items
+
+    end
+    
+    private
+
+    def receipt
+      @receipt || nil
+    end
   end
 end
