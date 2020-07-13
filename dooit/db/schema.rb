851112cd0b7b11e0b39d8738aef681f593ae0a36
odilialirani <oclirani@gmail.com>
2020-07-13 13:27:48 -0500

    db

--- dooit/db/schema.rb
+++ dooit/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_31_212621) do
+ActiveRecord::Schema.define(version: 2020_07_13_181057) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,24 @@ ActiveRecord::Schema.define(version: 2020_05_31_212621) do
     t.string "checksum", null: false
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "budget_categories", force: :cascade do |t|
+    t.boolean "active"
+    t.string "title"
+  end
+
+  create_table "budget_spendings", force: :cascade do |t|
+    t.string "location"
+    t.date "date"
+    t.integer "amount"
+  end
+
+  create_table "budgets", force: :cascade do |t|
+    t.integer "amount"
+    t.date "start_date"
+    t.date "end_date"
+    t.boolean "active"
   end
 
   create_table "friends", force: :cascade do |t|
