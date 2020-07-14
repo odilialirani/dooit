53a817890d042224c9ca16cc718d19cc3f499525
odilialirani <oclirani@gmail.com>
2020-07-13 20:41:43 -0500

    budget-category relation

--- dooit/app/models/budget/budget.rb
+++ dooit/app/models/budget/budget.rb
@@ -2,6 +2,14 @@ module Budget
   class Budget < ApplicationRecord
     self.table_name = 'budgets'
 
+    validates :category_id, presence: true
+    validate :no_active_budget
+
+
     belongs_to :category, class_name: 'Budget::Category', foreign_key: 'category_id'
+
+    def no_active_budget
+      # there should only be 1 budget per category at any time
+    end
   end
 end
--- dooit/app/models/budget/category.rb
+++ dooit/app/models/budget/category.rb
@@ -3,5 +3,7 @@ module Budget
     self.table_name = 'budget_categories'
 
     belongs_to :user
+
+    has_many :budgets, class_name: 'Budget::Budget', foreign_key: 'category_id'
   end
 end
